ff3082e3a369a469d66a89889c776a3aa39e5ecb
123AbhiSahu123 <sahuabhishek3810@gmail.com>
2026-07-21 23:52:07 +0530

    fix route

--- routes/sturoutes.js
+++ routes/sturoutes.js
@@ -6,6 +6,6 @@ const router = express.Router();
 router.post('/register', registerUser);
 router.post('/login', loginUser);
 router.post('/data', verifyToken, registerData);
-router.get('/profile/:userId', verifyToken, getProfile);
+router.get('/profile', verifyToken, getProfile);
 
 export default router;
