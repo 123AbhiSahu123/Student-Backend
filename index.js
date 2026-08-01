9db37a94bfea38fc223bcc300f483c9f51e1227f
123AbhiSahu123 <sahuabhishek3810@gmail.com>
2026-08-02 00:37:33 +0530

    temp

--- index.js
+++ index.js
@@ -9,10 +9,10 @@ const app = express();
 
 
 //temporary log see 
-app.use((req, res, next) => {
-     console.log(req.method, req.url);
-     next();
-});
+// app.use((req, res, next) => {
+//      console.log(req.method, req.url);
+//      next();
+// });
 
 app.use(cors({
      origin: ["http://localhost:5173", "https://student-frontend-alpha-lyart.vercel.app"],
